946398bd88e83c93f1c4bddba020ef501209b3d5
Micahliu <lwt1732@gmail.com>
2013-11-27 16:25:32 -0500

    Milestone 5: Complete the implementation of NameSurferGraph class
    Milestone 5: Complete the implementation of NameSurferGraph class

--- Assignment5/NameSurferGraph.java
+++ Assignment5/NameSurferGraph.java
@@ -13,20 +13,21 @@ import java.awt.*;
 
 public class NameSurferGraph extends GCanvas
 	implements NameSurferConstants, ComponentListener {
-
+	private ArrayList<NameSurferEntry> drawList = new ArrayList<NameSurferEntry>();
 	/**
 	* Creates a new NameSurferGraph object that displays the data.
 	*/
 	public NameSurferGraph() {
 		addComponentListener(this);
-		//	 You fill in the rest //
+		
 	}
 	
 	/**
 	* Clears the list of name surfer entries stored inside this class.
 	*/
 	public void clear() {
-		//	 You fill this in //
+	      drawList.clear();
+	      update();
 	}
 	
 	/* Method: addEntry(entry) */
@@ -36,7 +37,8 @@ public class NameSurferGraph extends GCanvas
 	* simply stores the entry; the graph is drawn by calling update.
 	*/
 	public void addEntry(NameSurferEntry entry) {
-		// You fill this in //
+        drawList.add(entry);
+        update();
 	}
 	
 	
@@ -49,11 +51,86 @@ public class NameSurferGraph extends GCanvas
 	* the size of the canvas changes.
 	*/
 	public void update() {
-		//	 You fill this in //
+		removeAll();
+	    drawGrid();
+	    for (int i=0; i<drawList.size(); i++){
+            drawEntry(drawList.get(i), i);
+        }
 	}
 	
-	
-	
+	public void drawGrid(){
+		double startY = 0;
+		double endY = getHeight();
+		
+		//draws the vertical lines
+		for(int i=0;i<NDECADES;i++){
+			double x = i *(getWidth()/NDECADES);
+			GLine line = new GLine(x, startY, x, endY);
+			add(line);
+		}
+		
+		//draws two horizontal lines in the graph
+		add(new GLine(0, GRAPH_MARGIN_SIZE, getWidth(), GRAPH_MARGIN_SIZE));
+        add(new GLine(0, endY-GRAPH_MARGIN_SIZE, getWidth(), endY-GRAPH_MARGIN_SIZE));
+        
+        //draws the decade labels
+        for(int i = 0; i<NDECADES; i++) {
+            int decade = START_DECADE;
+            decade += 10*i;
+            String label = Integer.toString(decade);
+            double y = getHeight() - GRAPH_MARGIN_SIZE/4;
+            double x = 2 + i * (getWidth()/NDECADES);
+            GLabel displayedDecade = new GLabel(label, x, y);
+            add(displayedDecade);
+        }
+	}
+	 private void drawEntry(NameSurferEntry entry, int entryNumber) {
+		 String name = entry.getName();
+		 for (int i=0; i<NDECADES; i++){
+			 int var_rank1 = entry.getRank(i);
+			 int var_rank2 = entry.getRank(i+1);
+			 
+			 double x1 = (getWidth()/NDECADES)*i;
+			 double x2 = (getWidth()/NDECADES)*(i+1);
+			 double y1= getY(var_rank1);
+			 double y2= getY(var_rank2);
+			 GLine line = new GLine(x1, y1, x2, y2);
+			 line.setColor(getColor(i));
+             add(line);
+             
+             if(var_rank1==0){
+            	 name= name + " *";
+             }else{
+            	 name = name + " " + var_rank1;
+             }
+             GLabel label = new GLabel(name);
+             label.setColor(getColor(i));
+             add(label, x1+3, y1-3);
+			 
+		 }
+		 
+	 }
+	 private double getY(int rank){
+		if(rank==0){
+			return (getHeight() - GRAPH_MARGIN_SIZE);
+		}else{
+			return (GRAPH_MARGIN_SIZE + (getHeight() - GRAPH_MARGIN_SIZE*2) * rank/MAX_RANK);
+		}
+	 }
+	 private Color getColor(int i){
+		  i = i%4;
+
+	      if(i==0){
+	    	  return Color.BLACK;
+	      }else if(i==1){
+	    	  return Color.RED;
+	      }else if(i==2){
+	    	  return Color.BLUE;
+	      }else {
+	    	  return Color.MAGENTA;
+	      }
+
+	 }
 	
 	/* Implementation of the ComponentListener interface */
 	public void componentHidden(ComponentEvent e) { }
